061a66e651b7ffe9db311a3bf6b23a820257de48
hanjx <hanjx@fenbi.com>
2020-09-10 11:14:19 +0800

    exoplayer demo by select video from local

--- ExoPlayerDemo/app/src/main/java/com/hanjx/exoplayerdemo/MainActivity.java
+++ ExoPlayerDemo/app/src/main/java/com/hanjx/exoplayerdemo/MainActivity.java
@@ -1,14 +1,58 @@
 package com.hanjx.exoplayerdemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQ_CODE_SELECT_VIDEO = 1997;
+
+    private PlayerView playerView;
+    private TextView selectBtn;
+
+    ExoPlayer exoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.player_view);
+        selectBtn = findViewById(R.id.select_btn);
+        selectBtn.setOnClickListener(v -> toSelect());
+        exoPlayer = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(exoPlayer);
+    }
+
+    private void toSelect() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQ_CODE_SELECT_VIDEO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQ_CODE_SELECT_VIDEO && data != null) {
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                    Util.getUserAgent(this, "com.hanjx.exoplayerdemo"));
+            MediaSource videoSource =
+                    new ProgressiveMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(data.getData());
+            exoPlayer.prepare(videoSource);
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
